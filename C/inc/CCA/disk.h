29bb356b44a35881da241a0b39be9cc3e7a41072
cc <cc@debian-dev.promethee.services>
2020-09-07 19:34:21 +0200

    Added disksize function

--- C/inc/CCA/disk.h
+++ C/inc/CCA/disk.h
@@ -1,4 +1,4 @@
-/*-                                               -*- c -*-
+/*-
  * Copyright (c) 2010
  *      Christian CAMIER <chcamier@free.fr>
  *
@@ -15,17 +15,11 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#ifndef __CC_IPCONV_H__
-#define __CC_IPCONV_H__
-struct sockaddr;
+#include <stdint.h>
 
-extern struct sockaddr *cc_address2ip (const char *, struct sockaddr *);
-extern struct sockaddr *cc_address2ip4(const char *, struct sockaddr *);
-extern struct sockaddr *cc_address2ip6(const char *, struct sockaddr *);
-#define CC_IPCONV_QUERY		0
-#define CC_IPCONV_IPV4ONLY	1
-#define CC_IPCONV_IPV6ONLY	2
-#define CC_IPCONV_IPV4FIRST	3
-#define CC_IPCONV_IPV6FIRST	4
-extern int              cc_ipconv_behaviour(int);
-#endif /*!__CC_IPCONV_H__*/
+#ifndef __CCA__DISK_H__
+#define __CCA__DISK_H__
+
+extern uint64_t cc_disk_getsize(const char *);
+
+#endif /*!__CCA__DISK_H__*/
